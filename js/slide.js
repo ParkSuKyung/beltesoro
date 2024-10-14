4abdba6e039a3b4e6bd1e3064a66ce9a6b79ec77
ParkSuKyung <jhsk0823@naver.com>
2024-10-14 14:15:03 +0900

    벨테소로
    메인 반응형까지

---
+++ js/slide.js
@@ -0,0 +1,40 @@
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+
+
+
+
+$(document).ready(function(){
+
+  $(".btn>li").each(function(num){
+      $(this).attr({
+          "data-num":num
+      });
+  });
+
+  $(".btn>li").click(function(){
+      var i = $(this).attr("data-num");
+      console.log(i);
+
+      //img slide
+      $(".img_wrap").animate({
+          left:-800 * i
+      });
+
+      //.btn>li addClass
+      $(".btn>li").removeClass("btn_active");
+      $(".btn>li").eq(i).addClass("btn_active");
+
+      // text>li addClass
+      $(".text>li").removeClass("text_active");
+      $(".text>li").eq(i).addClass("text_active");
+  });
+
+
+
+});//end
